c6515cd7e0212e137d2de5f3a2828c0fcc955454
drppi44 <multinigel@gmail.com>
2015-09-01 16:39:31 +0300

    t:1 migrations problem

--- apps/hello/migrations/0005_load_myfixture.py
+++ apps/hello/migrations/0005_load_myfixture.py
@@ -4,10 +4,11 @@ from south.db import db
 from south.v2 import DataMigration
 from django.db import models
 
+
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        "Write your forwards methods here."
+        """ Write your forwards methods here."""
         # Note: Don't use "from appname.models import ModelName".
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
@@ -15,7 +16,7 @@ class Migration(DataMigration):
         call_command("loaddata", "initial_data.json")
 
     def backwards(self, orm):
-        "Write your backwards methods here."
+        """ Write your backwards methods here."""
 
     models = {
         u'hello.mydata': {
