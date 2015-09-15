c5a87f556005dc3349d9be49545aa5d6f67d01e6
drppi44 <multinigel@gmail.com>
2015-09-15 22:28:27 +0300

    ticket:5 image scale

---
+++ apps/hello/migrations/0004_auto__add_field_mydata_thumbnail.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'MyData.thumbnail'
+        db.add_column(u'hello_mydata', 'thumbnail',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=500, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'MyData.thumbnail'
+        db.delete_column(u'hello_mydata', 'thumbnail')
+
+
+    models = {
+        u'hello.mydata': {
+            'Meta': {'object_name': 'MyData'},
+            'avatar': ('django.db.models.fields.files.ImageField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['hello']
