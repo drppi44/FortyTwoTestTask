2b140dbb80cace997d783fc467cbba171087ced8
drppi44 <multinigel@gmail.com>
2015-10-22 09:24:21 +0300

    ticket:8 model signal

---
+++ apps/hello/migrations/0003_auto__add_modelsignal.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ModelSignal'
+        db.create_table(u'hello_modelsignal', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('model', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('action', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'hello', ['ModelSignal'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ModelSignal'
+        db.delete_table(u'hello_modelsignal')
+
+
+    models = {
+        u'hello.modelsignal': {
+            'Meta': {'object_name': 'ModelSignal'},
+            'action': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'hello.mydata': {
+            'Meta': {'object_name': 'MyData'},
+            'avatar': ('django_resized.forms.ResizedImageField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['hello']
