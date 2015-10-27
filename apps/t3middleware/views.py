011ae9025a2abb266e72437e074e5ae75417eda3
drppi44 <multinigel@gmail.com>
2015-10-27 11:52:27 +0200

    ticket:13 priority in teamplate

--- apps/t3middleware/views.py
+++ apps/t3middleware/views.py
@@ -1,19 +1,19 @@
 import json
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template.loader import render_to_string
 from .models import MyHttpRequest
-from django.shortcuts import render_to_response
 
 
 def request_view(request, template='request.html'):
     MyHttpRequest.objects.filter(is_viewed=False).update(is_viewed=True)
 
-    return render_to_response(template)
+    return render(request, template)
 
 
 def get_requests(request):
     """ return  10 last Http Requests from DB and not viewed count"""
-    ten_requests = MyHttpRequest.objects.all().order_by('-time')[:10]
+    ten_requests = MyHttpRequest.objects.all()[:10]
     data = dict(
         count=MyHttpRequest.objects.filter(is_viewed=False).count(),
         text=render_to_string('table_for_requests.html',
