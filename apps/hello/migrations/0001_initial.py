53ba1869836bf38ce464a9d6453eb23c9675f971
drppi44 <multinigel@gmail.com>
2015-11-11 17:50:08 +0200

    ticket:14 new db

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -7,7 +7,6 @@ from django.db import models
 
 class Migration(SchemaMigration):
     def forwards(self, orm):
-
         # Adding model 'UserProfile'
         db.create_table(u'hello_userprofile', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
