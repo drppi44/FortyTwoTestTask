20e320410b7d15a44435fb11a903c85050003561
drppi44 <multinigel@gmail.com>
2015-11-13 03:23:52 +0200

    ticket:14 new db

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -6,6 +6,7 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+
     def forwards(self, orm):
         # Adding model 'UserProfile'
         db.create_table(u'hello_userprofile', (
